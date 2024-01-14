5d773846a39f9f59f91ff4df07bc21d57c1b8799
Md Arafat <130009990+mdearafat@users.noreply.github.com>
2024-01-14 17:07:05 -0500

    Created Board and Square Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+export default function Board() {
+  return (
+    <>
+      <div>
+        <Square value="O" />
+        <Square value="X" />
+        <Square value="X" />
+      </div>
+      <div>
+        <Square value="X" />
+        <Square value="X" />
+        <Square value="X" />
+      </div>
+      <div>
+        <Square value="X" />
+        <Square value="X" />
+        <Square value="X" />
+      </div>
+    </>
+  );
+}
+
+function Square({ value }) {
+  return (
+    <button className="bg-white h-12 w-12 border border-gray-600 m-1 text-lg">
+      {value}
+    </button>
+  );
 }
